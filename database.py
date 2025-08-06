089012d2676b1284d683a656225c49c7fc75377e
Aryan Ghadekar <aryanghadekar@gmail.com>
2025-08-07 02:04:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
-engine=create_engine('postgresql://postgres:Shilpa1966@host:5432/Bajaj',echo=True)
+engine=create_engine('postgresql://postgres.obitimvwpybuhnhscdcr:Pranav12412770@aws-0-ap-south-1.pooler.supabase.com:5432/postgres')
 
 Base=declarative_base()
+
 
 Session=sessionmaker()
